da763eb745cb0e095c251bb607a6fe7f749ecb9b
javidellacosta <jdellacosta@encodesa.com.ar>
2024-03-08 00:55:48 -0300

    filtros

--- src/app/pages/clubes-view/clubes-view.component.ts
+++ src/app/pages/clubes-view/clubes-view.component.ts
@@ -3,6 +3,8 @@ import { ClubsService } from '../../services/club.service';
 import { ClubData } from 'src/app/interfaces/club';
 import { Router } from '@angular/router';
 import { Pagination } from 'src/app/interfaces/paginations';
+import { Tournament } from 'src/app/interfaces/tournament';
+import { TournamentsService } from 'src/app/services/tournament.service';
 
 @Component({
   selector: 'app-clubes-view',
@@ -12,6 +14,8 @@ import { Pagination } from 'src/app/interfaces/paginations';
 export class ClubesViewComponent implements OnInit {
 
   public clubs: ClubData[] = [];
+  public tournaments: Tournament[] = [];
+  public selectedTournamentId: any;
   public nameFilter?: string;
   public idTournamentFilter!: number;
   public itemsPerPage: number = 5;
@@ -27,23 +31,40 @@ export class ClubesViewComponent implements OnInit {
   constructor(
     private clubService: ClubsService,
     private router: Router,
+    private tournamentService: TournamentsService
   ) { }
 
   ngOnInit(): void {
     this.getClubs();
+    this.getTournaments();
   }
 
   getClubs() {
+    if(this.selectedTournamentId)
+    {
+      let id = this.selectedTournamentId.idTournament;
+      this.idTournamentFilter = id;
+    }
     this.clubService.GetClubs(
       this.nameFilter ? this.nameFilter : null,
       this.idTournamentFilter ? this.idTournamentFilter : null,
       this.Pagination).subscribe((resp) => {
+        if (!resp.response) {
+          this.clubs = [];
+        }
       this.clubs = resp.response.lstClubsGetDto;
       this.totalQuantity = resp.response.totalQuantity;
       this.numberOfPages = resp.response.numberOfPages;
         this.clubs.forEach((club) => {
           this.getImage(club);
         });
+    });
+  }
+
+  getTournaments()
+  {
+    this.tournamentService.GetTournaments().subscribe((resp) => {
+      this.tournaments = resp.response;
     });
   }
 
